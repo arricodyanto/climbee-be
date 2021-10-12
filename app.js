7239e2765abb6c6505b4ded3f366e533769e2aac
arrricodyanto <arricohandyanto@gmail.com>
2021-10-12 17:55:40 +0700

    update modul & navbar

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const morgan = require('morgan');
 const colors = require('colors');
-const dotenv = require('dotenv');
+const expressLayouts = require('express-ejs-layouts');
+const { body, validationResult } = require('express-validator');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
 
-dotenv.config();
+// Impor data
+
 
 // Initialize express
 const app = express();
@@ -16,15 +20,55 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
+// Third-party Middleware
+app.use(expressLayouts);
+
 // Routes
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.render('index', {
+        layout: 'layouts/main-layout',
+        title: 'Climbee - Fashion Store',
+        active: 'active'
+    })
+})
+app.get('/tentang', (req, res) => {
+    res.render('tentang', {
+        layout: 'layouts/main-layout',
+        title: 'Tentang Toko - Climbee',
+        active: 'active'
+    })
+})
+app.get('/produk', (req, res) => {
+    res.render('produk', {
+        layout: 'layouts/main-layout',
+        title: 'Semua Produk - Climbee',
+        active: 'active'
+    })
+})
+app.get('/mostlove', (req, res) => {
+    res.render('mostlove', {
+        layout: 'layouts/main-layout',
+        title: 'Most Love - Climbee',
+        active: 'active'
+    })
+})
+app.get('/pembayaran', (req, res) => {
+    res.render('pembayaran', {
+        layout: 'layouts/main-layout',
+        title: 'Pembayaran - Climbee',
+        active: 'active'
+    })
+})
+
+app.get('/', (req, res) => {
+    res.status(404)
+    res.send('<h1>404</404>')
 })
 
 // Set-up
-if (process.env.NODE_ENV === "development") {
-    app.use(morgan('dev'));
-}
+// if (process.env.NODE_ENV === "development") {
+//     app.use(morgan('dev'));
+// }
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`.yellow.bold)
