447f72457fd59815e672c3969ca74957136a54f3
arrricodyanto <arricohandyanto@gmail.com>
2021-10-14 09:33:43 +0700

    add login register system

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
 
 const express = require('express');
 const colors = require('colors');
 const expressLayouts = require('express-ejs-layouts');
 const { body, validationResult } = require('express-validator');
 const cookieParser = require('cookie-parser');
-const flash = require('connect-flash');
+const flash = require('express-flash');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const methodOverride = require('method-override')
+
+const initializePassport = require('./passport-config')
+initializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+)
 
 // Impor data
-
+const users = []
 
 // Initialize express
 const app = express();
@@ -18,14 +31,22 @@ app.set('view engine', 'ejs');
 
 // Built-in Middleware
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
-
+app.use(express.urlencoded({ extended: false }));
 
 // Third-party Middleware
 app.use(expressLayouts);
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
 
 // Routes
-app.get('/', (req, res) => {
+app.get('/', checkAuthenticated, (req, res) => {
     res.render('index', {
         layout: 'layouts/main-layout',
         title: 'Climbee - Fashion Store',
@@ -60,12 +81,56 @@ app.get('/pembayaran', (req, res) => {
         active: 'active'
     })
 })
-app.get('/login', (req, res) => {
+app.get('/login', checkNotAuthenticated, (req, res) => {
     res.render('login', {
         layout: 'layouts/login-layout',
         title: 'Login'
     })
 })
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register', {
+        layout: 'layouts/login-layout',
+        title: 'Register'
+    })
+})
+app.post('/register', checkNotAuthenticated, async(req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch {
+        res.redirect('/register')
+    }
+})
+app.delete('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/login')
+})
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+
+    res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/')
+    }
+    next()
+}
 
 app.get('/', (req, res) => {
     res.status(404)
